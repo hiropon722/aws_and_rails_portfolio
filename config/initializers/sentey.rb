e6f734eda866d093ac3f7d7eea51c8568e3ad37f
EC2 Default User <ec2-user@ip-172-31-27-212.ap-northeast-1.compute.internal>
2023-09-06 15:44:26 +0900

    sentry_renamed

--- config/initializers/sentey.rb
+++ config/initializers/sentey.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ''
+  config.dsn = 'https://9a0ffdb271bef5286720aa81f18f703a@o4505832069857280.ingest.sentry.io/4505832078835712'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
